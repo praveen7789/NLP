d72ebc7278b5d5bdc6299ab8431223ec84eb6725
praveen7789 <bpraveen7789@gmail.com>
2019-06-05 19:29:54 +0530

    NLP.zip

---
+++ Desktop/ML Andrew NG/Srekanth sir class 2/Advanced ML/Support Vector Machine/plotting.py
@@ -0,0 +1,22 @@
+# Visualising the Training set results
+plt.figure(figsize =(8,8))
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, model_svm.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.25, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], 
+               color = ['red', 'green'][i], label = j)
+
+#plt.scatter(X_set[:,0],X_set[:,1] ,color = 'blue')
+    
+plt.title('K Nearest Neighbour (testing set)')
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.legend()
+plt.show()
